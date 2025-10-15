da797cfd9c30b59343533417002b2f1d0e22dde5
TLTP-SIZEMATCH <tom.waddell@uhn.ca>
2025-10-14 22:27:05 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,3 @@
 self.addEventListener("install", e=>{e.waitUntil(caches.open("tltp-calc-v2").then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"])))});
 self.addEventListener("fetch", e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+
